b44ea3a688ce40a8361e7ecd8e1dda17bbad2d8e
PenguinVEVO <noobhunt101@gmail.com>
2023-12-23 17:16:02 +0800

    QuitGame trigger now also quits editor playmode

--- Assets/Scripts/QuitApplication.cs
+++ Assets/Scripts/QuitApplication.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEditor;
 
 public class QuitApplication : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
 	{
-		Application.Quit();
-	}
-
+#if UNITY_EDITOR
+        {
+            EditorApplication.isPlaying = false;
+        }
+#else
+        {
+            Application.Quit();
+        }
+#endif
+    }
 }
